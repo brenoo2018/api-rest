3f3f9062453abec8bd69d3fc113252e1f08c88c4
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-04-24 13:12:38 -0300

    initial commit and configurations for database and fastify

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+import { knex } from './database';
+
+const app = fastify();
+
+app.get('/hello', async () => {
+  const teste = await knex('sqlite_schema').select('*');
+  return teste;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('http server running');
+});
