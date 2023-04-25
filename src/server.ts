d70c01cec82e9e9bbd83b4000346560991ff50fd
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-04-25 07:43:51 -0300

    creating automated test to create transaction

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,4 @@
-import fastify from 'fastify';
-import cookie from '@fastify/cookie';
-import { transactionsRoutes } from './routes/transactions';
-
-const app = fastify();
-
-app.register(cookie);
-
-// app.addHook('preHandler', async (req, res) => {
-
-// })
-
-app.register(transactionsRoutes, { prefix: 'transactions' });
+import { app } from './app';
 
 app.listen({ port: 3333 }).then(() => {
   console.log('http server running');
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,6 +1,25 @@
-import { test } from 'vitest';
+import { expect, test, beforeAll, afterAll } from 'vitest';
+import request from 'supertest';
 
-test('', () => {
+import { app } from '../src/app';
+
+beforeAll(async () => {
+  await app.ready();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+test('user can create a new transaction', async () => {
   //executar a função
   //esperar que a função tenha o resultado desejado
+
+  const response = await request(app.server).post('/transactions').send({
+    title: 'new transactions',
+    amount: 500,
+    type: 'credit',
+  });
+
+  expect(response.statusCode).toEqual(201);
 });
